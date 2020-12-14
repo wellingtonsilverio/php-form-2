2e323efc287c2ad58715ba38e787d8ed715141f6
Wellington Silverio <wellington.alves.silverio@gmail.com>
2020-12-14 20:02:57 -0300

    init

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+  $name = $_POST["nome"];
+  $ra = $_POST["ra"];
+  $sex = $_POST["sexo"];
+  $years = $_POST["idade"];
+  $address = $_POST["endereco"];
+  $phone = $_POST["telefone"];
+  $mail = $_POST["mail"];
+
+  if ($name && $ra && $sex && $years && $address && $phone && $mail) {
+    $file = fopen('file.bin','a');
+    if ($file == false) {
+      $error = array('1', 'VNão foi possível criar o arquivo.');
+    } else {
+      if (!fwrite($file, $name + ", " + $ra + ", " + $sex + ", " + $years + ", " + $address + ", " + $phone + ", " + $mail + ";")) {
+        $error = array('1', 'Não foi possível atualizar o arquivo.');
+      } else {
+        $redirect = 1;
+      }
+
+      fclose($arquivo);
+    }
+  } else if ($name || $ra || $sex || $years || $address || $phone || $mail) {
+    $error = array('1', 'Você precisa preencher o formulario completo.');
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Atividade 8: PHP - Formulário de cadastro</title>
+  <link rel="stylesheet" href="index.styles.css">
+</head>
+<body>
+<div class="login-box">
+  <h2>Formulário de cadastro de aluno</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="nome" required="">
+      <label>Nome</label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="ra" required="">
+      <label>RA</label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="sexo" required="">
+      <label>Sexo</label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="idade" required="">
+      <label>Idade</label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="endereco" required="">
+      <label>Endereço</label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="telefone" required="">
+      <label>Telefone</label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="mail" required="">
+      <label>e-mail</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      CADASTRAR
+    </a>
+  </form>
+</div>
+</body>
+</html>
